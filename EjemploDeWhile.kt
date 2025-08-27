3e9a4e230fa5e58bb49e0c408a30ff34023c5a37
BenjaRami <benjaramirez261@gmail.com>
2025-08-27 15:50:16 -0400

    Whiles

---
+++ EjemploDeWhile.kt
@@ -0,0 +1,20 @@
+/*
+fun main (){
+    var i=1
+    var ciclo =1
+    while (i<=100){
+        val numRandom=(0..10).random()
+        i=i+numRandom
+        println("ciclo $ciclo = $i")
+        ciclo++
+    }
+}*/
+fun main() {
+    var i = 1
+    while (i<=100){
+        if (i%2 !=0){ //esto para los impares, para los pares sería (i%2==0)
+            println("i: $i")
+        }
+        i++
+    }
+}
